b6a7f11dcd8b91c506cb7b6ec86a2e938b0e68bd
Chase May <cmay11@mail.csuchico.edu>
2020-11-16 19:33:20 -0800

    Finished creating tests for each function

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -14,9 +14,73 @@ class PracticeTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
-TEST(PracticeTest, is_simple_palindrome)
+//isPalindrome tests
+
+TEST(PracticeTest, is_not_simple_palindrome)
 {
     Practice obj;
     bool actual = obj.isPalindrome("Not a palindrome");
+    ASSERT_FALSE(actual);
+}
+
+TEST(PracticeTest, is_simple_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("racecar");
     ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, is_not_symbol_palindrome)
+{
+	Practice obj;
+	bool actual = obj.isPalindrome("$$$%%%");
+	ASSERT_FALSE(actual);
+}
+
+TEST(PracticeTest, is_same_letter_palindrome)
+{
+	Practice obj;
+	bool actual = obj.isPalindrome("a");
+	ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, is_same_number_palindrome)
+{
+	Practice obj;
+	bool actual = obj.isPalindrome("9");
+	ASSERT_TRUE(actual);
+}
+
+//sortDescending Tests
+
+TEST(sortDescending, is_descending_order)
+{
+	Practice obj;
+	int first(1), second(2), third(3);
+	bool actual = obj.sortDescending(first, second, third);
+	ASSERT_TRUE(actual);
+}
+
+TEST(sortDescending, is_same_number_descending_order)
+{
+	Practice obj;
+	int first(2), second(3), third(3);
+	bool actual = obj.sortDescending(first, second, third);
+	ASSERT_FALSE(actual);
+}
+
+TEST(sortDescending, is_descending_order_big_numbers)
+{
+	Practice obj;
+	int first(994323232), second(2521353), third(17);
+	bool actual = obj.sortDescending(first, second, third);
+	ASSERT_TRUE(actual);
+}
+
+TEST(sortDescending, is_acsending_order_big_numbers)
+{
+	Practice obj;
+	int first(7384), second(938472), third(63028496);
+	bool actual = obj.sortDescending(first, second, third);
+	ASSERT_TRUE(actual);
 }
